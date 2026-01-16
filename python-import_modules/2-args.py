7d5cd3e77db030af3695efd43947e99b2c5e5657
Handroc <andricassani42069@gmail.com>
2026-01-16 20:03:37 +0100

    zdzd

--- python-import_modules/2-args.py
+++ python-import_modules/2-args.py
@@ -6,7 +6,10 @@ if __name__ == "__main__":
     j = 1
     if i == 0:
         print("{0} arguments.".format(i))
-    else:
+    if i == 1:
+        print("{0} argument:".format(i))
+        print("{0}: {1}".format(1, sys.argv[1]))
+    if i > 1:
         print("{0} arguments:".format(i))
         for j in range(1, i + 1):
             print("{0}: {1}".format(j, sys.argv[j]))
--- python-import_modules/3-infinite_add.py
+++ python-import_modules/3-infinite_add.py
@@ -4,7 +4,7 @@ import sys
 if __name__ == "__main__":
     i = len(sys.argv) - 1
     sum = 0
-    for j in range(1,i + 1):
+    for j in range(1, i + 1):
         num = int(sys.argv[j])
         sum += num
     print("{0}".format(sum))
