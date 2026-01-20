1068a03f1bbd6a06e4cfb839a3354f58580aaf99
Handroc <andricassani42069@gmail.com>
2026-01-20 14:09:49 +0100

    another new commit message

--- python-more_data_structures/12-roman_to_int.py
+++ python-more_data_structures/12-roman_to_int.py
@@ -1,3 +1,4 @@
+#!/usr/bin/python3
 def roman_to_int(roman_string):
     if type(roman_string) is not str or not roman_string:
         return 0
--- python-more_data_structures/9-multiply_by_2.py
+++ python-more_data_structures/9-multiply_by_2.py
@@ -1,3 +1,4 @@
+#!/usr/bin/python3
 def multiply_by_2(a_dictionary):
     keys_dic = list(a_dictionary)
     dic_two = {keys: a_dictionary[keys]*2 for keys in keys_dic}
