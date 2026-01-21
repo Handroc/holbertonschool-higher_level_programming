ad02c975bbd9de82e9231ca630da3a6eea21a395
Handroc <andricassani42069@gmail.com>
2026-01-21 13:29:36 +0100

    d

--- python-more_data_structures/12-roman_to_int.py
+++ python-more_data_structures/12-roman_to_int.py
@@ -5,9 +5,10 @@ def roman_to_int(roman_string):
     ro_dict = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
     num = 0
     for i in range(1, len(roman_string)):
-        if ro_dict[roman_string[i]] <= ro_dict[roman_string[i - 1]]:
-            num += ro_dict[roman_string[i - 1]]
+        a = ro_dict[roman_string[i - 1]]
+        if ro_dict[roman_string[i]] <= a:
+            num += a
         else:
-            num -= ro_dict[roman_string[i - 1]]
+            num -= a
     num += ro_dict[roman_string[-1]]
     return num
