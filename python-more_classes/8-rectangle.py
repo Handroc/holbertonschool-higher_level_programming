ee6e725fe8fea8c2932083b10b8c48165c847970
Handroc <andricassani42069@gmail.com>
2026-01-28 11:44:43 +0100

    feat: class makes rectangle and tells if one is bigger than another

--- python-more_classes/8-rectangle.py
+++ python-more_classes/8-rectangle.py
@@ -89,6 +89,7 @@ class Rectangle:
 
     @staticmethod
     def bigger_or_equal(rect_1, rect_2):
+        """Function that returns the biggest rectangle by area"""
         msg_err_ins1 = "rect_1 must be an instance of Rectangle"
         msg_err_ins2 = "rect_2 must be an instance of Rectangle"
         if not (isinstance(rect_1, Rectangle)):
