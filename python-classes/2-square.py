9865965b7e47c6a86e15e70eac432e4d6adb9a31
Handroc <andricassani42069@gmail.com>
2026-01-28 10:03:56 +0100

    fixed documentation

--- python-classes/2-square.py
+++ python-classes/2-square.py
@@ -7,12 +7,7 @@ Square
 class Square:
     """Class that defines a square by size"""
     def __init__(self, size=0):
-        """Function that initializes the size.
-        
-        Args:
-            size (int, optional): Defaults to 0
-                size of the square
-        """
+        """Function that initializes the size."""
         msg_err = "size must be an integer"
         msg_err_size = "size must be >= 0"
         if type(size) is not int:
