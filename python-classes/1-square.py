c4a3a04684c0bd04078b50b2a7d990476b56ca3d
Handroc <andricassani42069@gmail.com>
2026-01-28 09:48:02 +0100

    d

--- python-classes/1-square.py
+++ python-classes/1-square.py
@@ -5,9 +5,10 @@ Docstring for 0-square.py
 
 
 class Square:
+    """Class Square that defines a square"""
     def __init__(self, size):
         """
-        defines a square
+        Docstring for __init__
         :param size: size of the square
         """
         self.__size = size
