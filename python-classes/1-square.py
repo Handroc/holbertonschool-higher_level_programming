21af98c3d697eb2c846521f152a6225833378d15
Handroc <andricassani42069@gmail.com>
2026-01-28 09:45:53 +0100

    d

--- python-classes/1-square.py
+++ python-classes/1-square.py
@@ -3,11 +3,10 @@
 Docstring for 0-square.py
 """
 
+
 class Square:
     def __init__(self, size):
         """
-        Docstring for __init__
         :param size: size of the square
         """
         self.__size = size
-
