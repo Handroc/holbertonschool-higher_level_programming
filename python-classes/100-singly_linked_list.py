edbf04dce71199773c19da6bd07f2004c6aa04fa
Handroc <andricassani42069@gmail.com>
2026-01-29 11:49:25 +0100

    feat: a usable singly linked list

--- python-classes/100-singly_linked_list.py
+++ python-classes/100-singly_linked_list.py
@@ -7,6 +7,7 @@ Singly linked list
 class Node:
     """Class that defines a singly linked list"""
     def __init__(self, data, next_node=None):
+        """Function that initializes the node"""
         msg_err_int = "data must be an integer"
         if not isinstance(data, int):
             raise TypeError(msg_err_int)
@@ -18,14 +19,17 @@ class Node:
 
     @property
     def data(self):
+        """Function that returns the data of the node"""
         return self.__data
 
     @property
     def next_node(self):
+        """Function that returns the next node"""
         return self.__next_node
 
     @data.setter
     def data(self, value):
+        """Function that sets the data of the node"""
         msg_err_int = "data must be an integer"
         if not isinstance(value, int):
             raise TypeError(msg_err_int)
@@ -33,6 +37,7 @@ class Node:
 
     @next_node.setter
     def next_node(self, value):
+        """Function that sets the next node"""
         msg_err_node = "next node must be a Node object"
         if not isinstance(value, Node) and value is not None:
             raise TypeError(msg_err_node)
@@ -42,9 +47,11 @@ class Node:
 class SinglyLinkedList:
     """Class that defines a singly linked list"""
     def __init__(self):
+        """Function that initializes the singly linked list"""
         self.__head = None
 
     def sorted_insert(self, value):
+        """Function that inserts a new Node to the list"""
         new_node = Node(value)
         if self.__head is None or self.__head.data > new_node.data:
             new_node.next_node = self.__head
@@ -57,6 +64,7 @@ class SinglyLinkedList:
         current_node.next_node = new_node
 
     def __str__(self):
+        """Function that defines the string representation of the list"""
         current_node = self.__head
         linklist = ""
         while current_node:
