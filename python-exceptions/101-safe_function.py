88fb2eafa45356a664baddb329567a6074449949
Handroc <andricassani42069@gmail.com>
2026-01-30 13:21:58 +0100

    b

--- python-exceptions/101-safe_function.py
+++ python-exceptions/101-safe_function.py
@@ -1,8 +1,11 @@
 #!/usr/bin/python3
 import sys
+
+
 def safe_function(fct, *args):
     try:
-        return fct(args[0], args[1])
+        funct = fct(args[0], args[1])
+        return funct
     except Exception as ne:
         print("Exception: {}".format(ne), file=sys.stderr)
         return None
