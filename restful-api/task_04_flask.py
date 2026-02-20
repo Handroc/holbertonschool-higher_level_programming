6433f38382f53b631969ff9f40459ed187edf1b2
Handroc <andricassani42069@gmail.com>
2026-02-20 14:21:46 +0100

    fix: update task_04_flask.py

--- restful-api/task_04_flask.py
+++ restful-api/task_04_flask.py
@@ -14,7 +14,7 @@ def home():
 def data():
     return jsonify(list(userdatabase.keys()))
 
-@app.route("/data/<username>")
+@app.route("/users/<username>")
 def user_data(username):
     name_lower = username.lower()
     if name_lower in userdatabase:
@@ -29,7 +29,7 @@ def status():
 @app.route("/add_user", methods=["POST"])
 def add_user():
     new_user = request.get_json(silent=True)
-    if not new_user:
+    if new_user is None:
         return ({"error" : "Invalid JSON"}), 400
     if "username" not in new_user:
         return ({"error" : "Username is required"}), 400
@@ -44,4 +44,4 @@ def add_user():
     }), 201
 
 if __name__ == "__main__":
-    app.run()
+    app.run(debug=True)
